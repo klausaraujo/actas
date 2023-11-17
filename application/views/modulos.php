738ab839830ddd37b5eb5488aaec05cca2b4e07c
klausaraujo <56495973+klausaraujo@users.noreply.github.com>
2023-11-16 23:27:49 -0500

    paisa

--- application/views/modulos.php
+++ application/views/modulos.php
@@ -3,7 +3,7 @@
 				<div class="iq-card-header d-flex justify-content-between">
 				   <div class="iq-header-title card-body ">
 				<h3 style="font-size:22px;" class="text-center">
-				   <b>Tablero de Control Estratégico de Aplicativos de la Red Prestacional Sabogal</b>
+				   <b>Tablero de Control Estratégico de Aplicativos</b>
 				</h3>
 					</div>
 				</div>
